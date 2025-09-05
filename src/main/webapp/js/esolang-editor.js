7acea98b017a8075af6fec951418d119438ffe71
loganKinzler <logankinzler@gmail.com>
2025-09-05 13:07:20 -0400

    Got JQuery with POST requests completed. Servlet takes in code & returns basic trace text.

--- src/main/webapp/js/esolang-editor.js
+++ src/main/webapp/js/esolang-editor.js
@@ -1,27 +1,24 @@
 let esolang_editor;
+let trace_output;
 
 $(function () {
     esolang_editor = document.getElementById("esolang-editor");
+    trace_output = document.getElementById("trace-output");
 });
 
 function compile($) {
-    const data = {
-        action: "compile",
-        test: "is works"
-    };
-
     fetch(contextPath + "/esolang_IDE", {
         method: "POST",
-        headers: {"Content-Type": "text/plain"},
-        body: "test"
+        headers: {"Content-Type": "plain/text"},
+        body: esolang_editor.value
     
     }).then(response => {
         if (!response.ok) throw new Error("HTTP Error: ${response.status}");
-        return response.json();
-    
-    }).then(data => {
-        console.log(data);
-    
+        return response.text();
+
+    }).then (body => {
+        trace_output.innerHTML = body;
+
     }).catch(error => {
         console.log("Error: ", error);
     });
