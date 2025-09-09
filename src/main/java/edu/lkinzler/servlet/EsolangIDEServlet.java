83b6843dbac434284daa52edddb15edd948917cf
loganKinzler <logankinzler@gmail.com>
2025-09-09 13:26:29 -0400

    Removed the try-catch blocks, updated whitespace, & shifted a few things around.

--- src/main/java/edu/lkinzler/servlet/EsolangIDEServlet.java
+++ src/main/java/edu/lkinzler/servlet/EsolangIDEServlet.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 
 public class EsolangIDEServlet extends HttpServlet {
+
     @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/esolang_IDE.jsp").forward(req, resp);
@@ -22,67 +23,46 @@ public class EsolangIDEServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // interpret request
-        BufferedReader reqBodyReader = req.getReader();
-        StringBuilder traceOutput = new StringBuilder();
-
         //Use the txt file in the project root, stops tomcat from creating a different one
         String projectPath = System.getProperty("user.dir");
         File userInputFile = new File(projectPath, "User_Input.txt");
-        //Debug test
-        System.out.println(userInputFile.getAbsolutePath());
 
         //Writing textarea string into a txt file
-        try{
-            FileWriter user_input = new FileWriter(userInputFile);
+        BufferedReader reqBodyReader = req.getReader();
+        StringBuilder traceOutput = new StringBuilder();
 
-            String line = reqBodyReader.readLine();
-            int lineNum = 1;
-
-            while (line != null) {
-                user_input.write(line);
-                line = reqBodyReader.readLine();
-                lineNum++;
-            }
-            user_input.close();
-        }
-        catch(IOException e){
-            System.out.println("file was not found.");
-        }
-
-        //Reading contents of the txt file
-        int lineNum = 1;
-        try{
-            //File User_Input = new File(projectPath, "User_Input.txt");
-            Scanner read = new Scanner(userInputFile);
-            while(read.hasNextLine()){
-                traceOutput.append(String.format("Line %d: %s\n", lineNum, read.nextLine()));
-                lineNum++;
-            }
-            read.close();
-        }
-        catch(FileNotFoundException e){
-            System.out.println("file was not found.");
-        }
-        /*
+        FileWriter user_input = new FileWriter(userInputFile);
         String line = reqBodyReader.readLine();
+
+
+
         int lineNum = 1;
 
         while (line != null) {
-            traceOutput.append( String.format("Line %d: %s\n", lineNum, line) );
+            user_input.write(line + "\n");
+            traceOutput.append(String.format("Line %d: %s\n", lineNum, line));
+
             line = reqBodyReader.readLine();
             lineNum++;
         }
 
+        // remove the last line (which is empty)
+        user_input.write("\b\b");
+
+        user_input.close();
         reqBodyReader.close();
 
-         */
+        //Reading contents of the txt file
+//        File User_Input = new File(projectPath, "User_Input.txt");
+//        Scanner read = new Scanner(userInputFile);
+//        read.close();
 
         // format response
         resp.setContentType("text/html");
-        PrintWriter respBodyWriter = resp.getWriter();
 
-        respBodyWriter.print( traceOutput.toString() );
+        // write
+        PrintWriter respBodyWriter = resp.getWriter();
+        respBodyWriter.print( traceOutput );
         respBodyWriter.close();
 	}
 }
